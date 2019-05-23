0e13bfff806df759f42d4f410cee5bf939351825
nidhineshnand <nnan773@aucklanduni.ac.nz>
2019-05-23 14:46:50 +1200

    Implemented hextat_complement method

---
+++ Testchecksum.py
@@ -0,0 +1,4 @@
+from checksum import hextat_complement
+
+v = hextat_complement(300)
+print(v)
--- checksum.py
+++ checksum.py
@@ -7,4 +7,8 @@ def internet_checksum(data, total=0x0):
     1. https://tools.ietf.org/html/rfc1071
     2. http://www.netfor2.com/checksum.html
     '''
-	pass # TODO: Implement this function and return the checksum
+	# TODO: Implement this function and return the checksum
+
+def hextat_complement(x):
+    mask = 0xffff
+    return bin(~x & mask)[2:]
